75f49aefed547d2f830f33e94ff4ee97f87ab37e
Andreas Cord-Landwehr <cola@uni-paderborn.de>
2012-06-07 16:30:13 +0200

    Cleanup FindKBibTeX cmake module.

--- cmake/modules/FindKBibTeX.cmake
+++ cmake/modules/FindKBibTeX.cmake
@@ -2,7 +2,7 @@
 # Once done this will define
 #
 #  KBIBTEX_FOUND - system has the kbibtex library
-#  KBIBTEX_INCLUDE_DIR - the kbibtex include directory
+#  KBIBTEX_INCLUDE_DIRS - the kbibtex include directory
 #  KBIBTEX_LIBRARIES - Link this to use the kbibtex library
 #
 # Copyright (c) 2012, Jörg Ehrichs, <joerg.ehrichs@gmx.de>
@@ -10,58 +10,74 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if (KBIBTEX_LIBRARIES AND KBIBTEX_INCLUDE_DIR)
-  # in cache already
-  set(KBIBTEX_FOUND TRUE)
-else (KBIBTEX_LIBRARIES AND KBIBTEX_INCLUDE_DIR)
+if( KBIBTEX_INCLUDE_DIRS AND KBIBTEX_LIBRARIES )
+    # Already in cache, be silent
+    set( KBIBTEX_FIND_QUIETLY TRUE )
+endif( KBIBTEX_INCLUDE_DIRS AND KBIBTEX_LIBRARIES )
 
-  find_path(KBIBTEX_INCLUDE_DIR kbibtexio_export.h
-    ${GNUWIN32_DIR}/include
-    PATH_SUFFIXES kbibtex
-  )
+find_library(
+    KBIBTEXIO_LIBRARY
+    NAMES kbibtexio
+    HINTS ${KDE4_LIB_DIR} ${LIB_INSTALL_DIR}
+)
 
-  find_library(KBIBTEX_LIBRARIES NAMES kbibtexgui
-    PATHS
-    ${GNUWIN32_DIR}/lib
-  )
-  find_library(KBIBTEX_LIBRARIES2 NAMES kbibtexio
-    PATHS
-    ${GNUWIN32_DIR}/lib
-  )
-  LIST(APPEND KBIBTEX_LIBRARIES ${KBIBTEX_LIBRARIES2})
-  
-  find_library(KBIBTEX_LIBRARIES3 NAMES kbibtexnetworking
-    PATHS
-    ${GNUWIN32_DIR}/lib
-  )
-  LIST(APPEND KBIBTEX_LIBRARIES ${KBIBTEX_LIBRARIES3})
-  
-  find_library(KBIBTEX_LIBRARIES4 NAMES kbibtexproc
-    PATHS
-    ${GNUWIN32_DIR}/lib
-  )
-  LIST(APPEND KBIBTEX_LIBRARIES ${KBIBTEX_LIBRARIES4})
-  
-  MESSAGE(STATUS "librarys" ${KBIBTEX_LIBRARIES})
-  
-    # set version variables
-    # todo find a way to do propper version check ...
-  set(KBIBTEX_VERSION "0.5.0")
-  STRING(REGEX REPLACE "^([0-9]+)\\.[0-9]+\\.[0-9]+.*" "\\1" KBIBTEX_VERSION_MAJOR "${KBIBTEX_VERSION}")
-  STRING(REGEX REPLACE "^[0-9]+\\.([0-9])+\\.[0-9]+.*" "\\1" KBIBTEX_VERSION_MINOR "${KBIBTEX_VERSION}")
-  STRING(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" KBIBTEX_VERSION_PATCH "${KBIBTEX_VERSION}")
+find_library(
+    KBIBTEXGUI_LIBRARY
+    NAMES kbibtexgui
+    HINTS ${KDE4_LIB_DIR} ${LIB_INSTALL_DIR}
+)
 
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(KBIBTEX DEFAULT_MSG KBIBTEX_INCLUDE_DIR KBIBTEX_LIBRARIES KBIBTEX_VERSION)
-  # ensure that they are cached
-  set(KBIBTEX_INCLUDE_DIR ${KBIBTEX_INCLUDE_DIR} CACHE INTERNAL "The kbibtex include path")
-  set(KBIBTEX_LIBRARIES ${KBIBTEX_LIBRARIES} CACHE INTERNAL "The libraries needed to use kbibtex")
-  
-  set(KBIBTEX_VERSION ${KBIBTEX_VERSION} CACHE INTERNAL "The full version string")
-  set(KBIBTEX_VERSION_MAJOR ${KBIBTEX_VERSION_MAJOR} CACHE INTERNAL "The major version string")
-  set(KBIBTEX_VERSION_MINOR ${KBIBTEX_VERSION_MINOR} CACHE INTERNAL "The minot version string")
-  set(KBIBTEX_VERSION_PATCH ${KBIBTEX_VERSION_PATCH} CACHE INTERNAL "The path version string")
-  
+find_library(
+    KBIBTEXNETWORKING_LIBRARY
+    NAMES kbibtexnetworking
+    HINTS ${KDE4_LIB_DIR} ${LIB_INSTALL_DIR}
+)
 
-endif (KBIBTEX_LIBRARIES AND KBIBTEX_INCLUDE_DIR)
+find_library(
+    KBIBTEXPROC_LIBRARY
+    NAMES kbibtexproc
+    HINTS ${KDE4_LIB_DIR} ${LIB_INSTALL_DIR}
+)
 
+find_path(
+    KBIBTEX_INCLUDE_DIRS
+    NAMES kbibtexio_export.h
+    HINTS ${KDE4_INCLUDE_DIR} ${INCLUDE_INSTALL_DIR}
+)
+
+if (KBIBTEXIO_LIBRARY AND KBIBTEXGUI_LIBRARY AND KBIBTEXNETWORKING_LIBRARY AND KBIBTEXPROC_LIBRARY AND KBIBTEX_INCLUDE_DIRS)
+    set(KBIBTEX_FOUND TRUE)
+    set(KBIBTEX_LIBRARIES ${KBIBTEXIO_LIBRARY} ${KBIBTEXGUI_LIBRARY} ${KBIBTEXNETWORKING_LIBRARY} ${KBIBTEXPROC_LIBRARY})
+endif(KBIBTEXIO_LIBRARY AND KBIBTEXGUI_LIBRARY AND KBIBTEXNETWORKING_LIBRARY AND KBIBTEXPROC_LIBRARY AND KBIBTEX_INCLUDE_DIRS)
+
+message(STATUS "libraries" ${KBIBTEX_LIBRARIES})
+
+# set version variables
+# todo find a way to do propper version check ...
+set(KBIBTEX_VERSION "0.5.0")
+STRING(REGEX REPLACE "^([0-9]+)\\.[0-9]+\\.[0-9]+.*" "\\1" KBIBTEX_VERSION_MAJOR "${KBIBTEX_VERSION}")
+STRING(REGEX REPLACE "^[0-9]+\\.([0-9])+\\.[0-9]+.*" "\\1" KBIBTEX_VERSION_MINOR "${KBIBTEX_VERSION}")
+STRING(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" KBIBTEX_VERSION_PATCH "${KBIBTEX_VERSION}")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(KBIBTEX DEFAULT_MSG KBIBTEX_INCLUDE_DIRS KBIBTEX_LIBRARIES KBIBTEX_VERSION)
+
+set(KBIBTEX_VERSION ${KBIBTEX_VERSION} CACHE INTERNAL "The full version string")
+set(KBIBTEX_VERSION_MAJOR ${KBIBTEX_VERSION_MAJOR} CACHE INTERNAL "The major version string")
+set(KBIBTEX_VERSION_MINOR ${KBIBTEX_VERSION_MINOR} CACHE INTERNAL "The minot version string")
+set(KBIBTEX_VERSION_PATCH ${KBIBTEX_VERSION_PATCH} CACHE INTERNAL "The path version string")
+
+if( KBIBTEX_FOUND_FOUND )
+    if( NOT KBIBTEX_FIND_QUIETLY )
+        message( STATUS "Found KBibTex libraries: ${KBIBTEX_LIBRARIES}" )
+    endif( NOT KBIBTEX_FIND_QUIETLY )
+else( KBIBTEX_FOUND_FOUND )
+    if( LibKBibTeX_FIND_REQUIRED )
+        message( FATAL_ERROR "Could not find KBibTeX libraries" )
+    endif( LibKBibTeX_FIND_REQUIRED )
+endif( KBIBTEX_FOUND_FOUND )
+
+mark_as_advanced(
+    KBIBTEX_INCLUDE_DIRS
+    KBIBTEX_LIBRARIES
+)
